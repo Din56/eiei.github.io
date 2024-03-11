aac9487f098b9a218584599e752c2f02c191384a
Din56 <151639804+Din56@users.noreply.github.com>
2024-03-12 02:06:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function Random(){
         button.style.display = "none"
         setTimeout(function() {
             button.style.display = "flex";
-        }, 2000); 
+        }, 2000);
     })
 
     var Card = {
@@ -17,6 +17,11 @@ function Random(){
         Card2 : document.getElementById("Card2"),
         Card3 : document.getElementById("Card3"),
         Card4 : document.getElementById("Card4"),
+        Card5 : document.getElementById("Card5"),
+        Card6 : document.getElementById("Card6"),
+        Card7 : document.getElementById("Card7"),
+        Card8 : document.getElementById("Card8"),
+        Card9 : document.getElementById("Card9"),
         SecretCard : document.getElementById("SecretCard"),
     }
     
@@ -26,6 +31,11 @@ function Random(){
         Rarely2 : document.getElementById("Rarely2"),
         Rarely3 : document.getElementById("Rarely3"),
         Rarely4 : document.getElementById("Rarely4"),
+        Rarely5 : document.getElementById("Rarely5"),
+        Rarely6 : document.getElementById("Rarely6"),
+        Rarely7 : document.getElementById("Rarely7"),
+        Rarely8 : document.getElementById("Rarely8"),
+        Rarely9 : document.getElementById("Rarely9"),
         SecretRarely : document.getElementById("SecretRarely"),
     }
 
@@ -39,27 +49,57 @@ function Random(){
     Rarely.Rarely3.style.display = 'none'
     Card.Card4.style.display = 'none'
     Rarely.Rarely4.style.display = 'none'
+    Card.Card5.style.display = 'none'
+    Rarely.Rarely5.style.display = 'none'
+    Card.Card6.style.display = 'none'
+    Rarely.Rarely6.style.display = 'none'
+    Card.Card7.style.display = 'none'
+    Rarely.Rarely7.style.display = 'none'
+    Card.Card8.style.display = 'none'
+    Rarely.Rarely8.style.display = 'none'
+    Card.Card9.style.display = 'none'
+    Rarely.Rarely9.style.display = 'none'
 
     Card.SecretCard.style.display = 'none'
     Rarely.SecretRarely.style.display = 'none'
 
-    RandomVaule = Math.floor(Math.random() * 101) // * 10 is 0 to 9 / * 11 is 0 to 10
+    RandomVaule = Math.floor(Math.random() * 1001) // * 10 is 0 to 9 / * 11 is 0 to 10
 
-    if (RandomVaule <= 50){
+    if (RandomVaule <= 500){
         Card.Card1.style.display = 'flex'
         Rarely.Rarely1.style.display = 'flex'
     }
-    else if (RandomVaule <= 75){
+    else if (RandomVaule <= 750){
         Card.Card2.style.display = 'flex'
         Rarely.Rarely2.style.display = 'flex'
     }
-    else if (RandomVaule <= 90){
+    else if (RandomVaule <= 900){
         Card.Card3.style.display = 'flex'
         Rarely.Rarely3.style.display = 'flex'
     }
-    else if (RandomVaule <= 100){
+    else if (RandomVaule <= 960){
         Card.Card4.style.display = 'flex'
         Rarely.Rarely4.style.display = 'flex'
+    }
+    else if (RandomVaule <= 980){
+        Card.Card5.style.display = 'flex'
+        Rarely.Rarely5.style.display = 'flex'
+    }
+    else if (RandomVaule <= 990){
+        Card.Card6.style.display = 'flex'
+        Rarely.Rarely6.style.display = 'flex'
+    }
+    else if (RandomVaule <= 995){
+        Card.Card7.style.display = 'flex'
+        Rarely.Rarely7.style.display = 'flex'
+    }
+    else if (RandomVaule <= 999){
+        Card.Card8.style.display = 'flex'
+        Rarely.Rarely8.style.display = 'flex'
+    }
+    else if (RandomVaule <= 1000){
+        Card.Card9.style.display = 'flex'
+        Rarely.Rarely9.style.display = 'flex'
     }
 
 }
@@ -71,6 +111,11 @@ function SummonSecret(){
         Card2 : document.getElementById("Card2"),
         Card3 : document.getElementById("Card3"),
         Card4 : document.getElementById("Card4"),
+        Card5 : document.getElementById("Card5"),
+        Card6 : document.getElementById("Card6"),
+        Card7 : document.getElementById("Card7"),
+        Card8 : document.getElementById("Card8"),
+        Card9 : document.getElementById("Card9"),
         SecretCard : document.getElementById("SecretCard"),
     }
     
@@ -80,6 +125,11 @@ function SummonSecret(){
         Rarely2 : document.getElementById("Rarely2"),
         Rarely3 : document.getElementById("Rarely3"),
         Rarely4 : document.getElementById("Rarely4"),
+        Rarely5 : document.getElementById("Rarely5"),
+        Rarely6 : document.getElementById("Rarely6"),
+        Rarely7 : document.getElementById("Rarely7"),
+        Rarely8 : document.getElementById("Rarely8"),
+        Rarely9 : document.getElementById("Rarely9"),
         SecretRarely : document.getElementById("SecretRarely"),
     }
 
@@ -93,8 +143,18 @@ function SummonSecret(){
     Rarely.Rarely3.style.display = 'none'
     Card.Card4.style.display = 'none'
     Rarely.Rarely4.style.display = 'none'
+    Card.Card5.style.display = 'none'
+    Rarely.Rarely5.style.display = 'none'
+    Card.Card6.style.display = 'none'
+    Rarely.Rarely6.style.display = 'none'
+    Card.Card7.style.display = 'none'
+    Rarely.Rarely7.style.display = 'none'
+    Card.Card8.style.display = 'none'
+    Rarely.Rarely8.style.display = 'none'
+    Card.Card9.style.display = 'none'
+    Rarely.Rarely9.style.display = 'none'
 
-    Card.SecretCard.style.display = 'flex'
-    Rarely.SecretRarely.style.display = 'flex'
+    Card.SecretCard.style.display = 'none'
+    Rarely.SecretRarely.style.display = 'none'
 
 }
