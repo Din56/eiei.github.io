2bb6446e3a857fb2cc84b63c94439c005e80c7b3
Din56 <151639804+Din56@users.noreply.github.com>
2024-03-12 23:34:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ console.log("Hey! What do you think you are doing");
 console.log("Dont try something crazy here >:)");
 console.log("And close this thing");
 
-function Random(){
+var StatCard = {
+    StatCard1 : 0,
+    StatCard2 : 0,
+    StatCard3 : 0,
+    StatCard4 : 0,
+    StatCard5 : 0,
+    StatCard6 : 0,
+    StatCard7 : 0,
+    StatCard8 : 0,
+    StatCard9 : 0,
+}
+
+var TotalCard = {
+    Total : 0
+}
+
+function Random(StatCard, TotalCard){
     document.getElementById("RandomButton").addEventListener("click", function() {
         var button = this;
         button.style.display = "none"
@@ -10,6 +26,10 @@ function Random(){
             button.style.display = "flex";
         }, 2000);
     })
+
+    TotalCard.Total = TotalCard.Total + 1
+    document.getElementById('TotalCard').innerHTML = '> You have random for ' +  TotalCard.Total  + ' times <'
+
 
     var Card = {
         StarterCard : document.getElementById("StarterCard"),
@@ -66,38 +86,65 @@ function Random(){
     RandomVaule = Math.floor(Math.random() * 1001) // * 10 is 0 to 9 / * 11 is 0 to 10
 
     if (RandomVaule <= 500){
+        StatCard.StatCard1 = StatCard.StatCard1 + 1
+        document.getElementById('ShowCard1').innerHTML = 'Card 1'
+        document.getElementById('StatCard1').innerHTML = 'x ' + StatCard.StatCard1
         Card.Card1.style.display = 'flex'
         Rarely.Rarely1.style.display = 'flex'
     }
     else if (RandomVaule <= 750){
+        StatCard.StatCard2 = StatCard.StatCard2 + 1
+        document.getElementById('ShowCard2').innerHTML = 'Card 2'
+        document.getElementById('StatCard2').innerHTML = 'x ' + StatCard.StatCard2
         Card.Card2.style.display = 'flex'
         Rarely.Rarely2.style.display = 'flex'
     }
     else if (RandomVaule <= 900){
+        StatCard.StatCard3 = StatCard.StatCard3 + 1
+        document.getElementById('ShowCard3').innerHTML = 'Card 3'
+        document.getElementById('StatCard3').innerHTML = 'x ' + StatCard.StatCard3
         Card.Card3.style.display = 'flex'
         Rarely.Rarely3.style.display = 'flex'
     }
     else if (RandomVaule <= 960){
+        StatCard.StatCard4 = StatCard.StatCard4 + 1
+        document.getElementById('ShowCard4').innerHTML = 'Card 4'
+        document.getElementById('StatCard4').innerHTML = 'x ' + StatCard.StatCard4
         Card.Card4.style.display = 'flex'
         Rarely.Rarely4.style.display = 'flex'
     }
     else if (RandomVaule <= 980){
+        StatCard.StatCard5 = StatCard.StatCard5 + 1
+        document.getElementById('ShowCard5').innerHTML = 'Card 5'
+        document.getElementById('StatCard5').innerHTML = 'x ' + StatCard.StatCard5
         Card.Card5.style.display = 'flex'
         Rarely.Rarely5.style.display = 'flex'
     }
     else if (RandomVaule <= 990){
+        StatCard.StatCard6 = StatCard.StatCard6 + 1
+        document.getElementById('ShowCard6').innerHTML = 'Card 6'
+        document.getElementById('StatCard6').innerHTML = 'x ' + StatCard.StatCard6
         Card.Card6.style.display = 'flex'
         Rarely.Rarely6.style.display = 'flex'
     }
     else if (RandomVaule <= 995){
+        StatCard.StatCard7 = StatCard.StatCard7 + 1
+        document.getElementById('ShowCard7').innerHTML = 'Card 7'
+        document.getElementById('StatCard7').innerHTML = 'x ' + StatCard.StatCard7
         Card.Card7.style.display = 'flex'
         Rarely.Rarely7.style.display = 'flex'
     }
     else if (RandomVaule <= 999){
+        StatCard.StatCard8 = StatCard.StatCard8 + 1
+        document.getElementById('ShowCard8').innerHTML = 'Card 8'
+        document.getElementById('StatCard8').innerHTML = 'x ' + StatCard.StatCard8
         Card.Card8.style.display = 'flex'
         Rarely.Rarely8.style.display = 'flex'
     }
     else if (RandomVaule <= 1000){
+        StatCard.StatCard9 = StatCard.StatCard9 + 1
+        document.getElementById('ShowCard9').innerHTML = 'Card 1'
+        document.getElementById('StatCard9').innerHTML = 'x ' + StatCard.StatCard9
         Card.Card9.style.display = 'flex'
         Rarely.Rarely9.style.display = 'flex'
     }
@@ -154,7 +201,26 @@ function SummonSecret(){
     Card.Card9.style.display = 'none'
     Rarely.Rarely9.style.display = 'none'
 
-    Card.SecretCard.style.display = 'none'
-    Rarely.SecretRarely.style.display = 'none'
+    Card.SecretCard.style.display = 'flex'
+    Rarely.SecretRarely.style.display = 'flex'
 
+    document.getElementById('ShowCardSecret').innerHTML = ':)'
+    document.getElementById('StatCardSecret').innerHTML = 'NICE'
+
+}
+
+function OpenInventory(){
+    var MainPage = document.getElementById("MainPage")
+    var InventoryPage = document.getElementById("InventoryPage")
+
+    MainPage.style.display = 'none'
+    InventoryPage.style.display = 'block'
+}
+
+function BackToMainPage(){
+    var MainPage = document.getElementById("MainPage")
+    var InventoryPage = document.getElementById("InventoryPage")
+
+    MainPage.style.display = 'block'
+    InventoryPage.style.display = 'none'
 }
